2b08cb5e10455ef1a8d01db3076c9cda66e532a2
Henri Menke <henri@icp.uni-stuttgart.de>
2020-01-27 13:20:56 +1300

    Qualify some things and remove copy and move constructors

--- fileptrbuf/fileptrbuf.hpp
+++ fileptrbuf/fileptrbuf.hpp
@@ -10,22 +10,22 @@
 
 #include <algorithm>
 #include <array>
+#include <cstdio>
 #include <ios>
-#include <stdexcept>
 #include <streambuf>
 
 class fileptrbuf : public std::streambuf {
-    ptrdiff_t const m_putback_size = 4UL;
+    std::size_t m_putback_size = 4UL;
     std::array<char_type, 4096> buffer;
-    FILE *m_f;
+    std::FILE *m_f;
     std::ios_base::openmode m_mode;
 
     int flush() {
-        if (!(m_mode & std::ios::out)) {
+        if (!(m_mode & std::ios::out) || m_f == nullptr) {
             return EOF;
         }
         std::size_t const num = pptr() - pbase();
-        std::size_t written = fwrite(pbase(), sizeof(char_type), num, m_f);
+        std::size_t written = std::fwrite(pbase(), sizeof(char_type), num, m_f);
         if (written != num) {
             return EOF;
         }
@@ -34,7 +34,7 @@ class fileptrbuf : public std::streambuf {
     }
 
 protected:
-    virtual int sync() override {
+    virtual int_type sync() override {
         if (flush() == EOF) {
             return -1;
         }
@@ -49,8 +49,17 @@ public:
         setp(buffer.data(), buffer.data() + buffer.size());
     }
 
+    fileptrbuf(fileptrbuf &&) = delete;
+    fileptrbuf(fileptrbuf const&) = delete;
+
+    ~fileptrbuf() {
+        sync();
+        m_f = nullptr;
+        m_mode = static_cast<std::ios_base::openmode>(0);
+    }
+
     virtual int_type underflow() override {
-        if (!(m_mode & std::ios::in)) {
+        if (!(m_mode & std::ios::in) || m_f == nullptr) {
             return EOF;
         }
         if (gptr() < egptr()) {
@@ -58,12 +67,13 @@ public:
         }
 
         // copy up to four characters previously read into the putback buffer
-        size_t const n_putback = std::min(gptr() - eback(), m_putback_size);
+        std::size_t const pos = gptr() - eback();
+        std::size_t const n_putback = std::min(pos, m_putback_size);
         std::copy_n(gptr() - n_putback, n_putback,
                     buffer.data() + m_putback_size - n_putback);
 
         // read new characters
-        size_t const num =
+        std::size_t const num =
             std::fread(buffer.data() + m_putback_size, sizeof(char_type),
                        buffer.size() - m_putback_size, m_f);
 
@@ -79,8 +89,9 @@ public:
         // Avoid EOF being casted to int -1
         return static_cast<unsigned char>(*gptr());
     }
-    virtual int_type overflow(int c) override {
-        if (!(m_mode & std::ios::out)) {
+
+    virtual int_type overflow(int_type c) override {
+        if (!(m_mode & std::ios::out) || m_f == nullptr) {
             return EOF;
         }
         if (c != EOF) {
